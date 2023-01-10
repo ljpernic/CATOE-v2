a587fa253ae1c46480b6859af9c761aaa05fad87
ljpernic <leonXperniciaro@gmail.com>
2023-01-10 10:57:09 -0500

    changing image sizes

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -51,7 +51,7 @@ export const IndexPageTemplate = ({
                   <div className="columns">
                     <div className="column is-12 has-text-centered">
                       <Link className="btn" to="/projects">
-                        See all projects
+                        See all projects 
                       </Link>
                     </div>
                   </div>
